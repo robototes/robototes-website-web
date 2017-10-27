cbdff9570fb6aa7d9d30f398d7c395bed6a9ad04
dannytech <dannytech@users.noreply.github.com>
2017-10-27 02:54:22 +0000

    Removed unnecessary error detection logic

--- server.js
+++ server.js
@@ -62,9 +62,6 @@ app.use(async (ctx, next) => {
     if (ctx.status >= 400) ctx.throw(ctx.status)
     else logHTTP(`\t--> ${ctx.status} OK`)
   } catch (err) {
-    // Make sure we have an error code
-    err.status = err.status || ctx.status
-
     // Render the error page
     ctx.render('error', {
       error: err
