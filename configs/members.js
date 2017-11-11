e4bb0d12b8db1af330f3af8dd3bc4d5e33dbe2b3
dannytech <dannytech@users.noreply.github.com>
2017-11-11 07:53:23 +0000

    Added programmers email Normalized member domains into one field Added integration with API for email redirects

--- configs/members.js
+++ configs/members.js
@@ -3,14 +3,17 @@ module.exports = {
     {
       name: 'The Team',
       alias: 'team',
-      domain: 'robototes',
-      tld: 'com'
+      domain: 'robototes.com'
     },
     {
       name: 'Website Developers',
       alias: 'webmaster',
-      domain: 'robototes',
-      tld: 'com'
+      domain: 'robototes.com'
+    },
+    {
+      name: 'Programmers',
+      alias: 'programmers',
+      domain: 'robototes.com'
     }
   ],
   teamMembers: [
@@ -18,57 +21,49 @@ module.exports = {
       name: 'Jacob Brouwer',
       role: 'Team Captian',
       alias: 's-brouwerj',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     },
     {
       name: 'Brielle Chenier',
       role: 'Outreach Lead',
       alias: 's-chenierb',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     },
     {
       name: 'Jonathan Schladetzky',
       role: 'Mechanical Lead',
       alias: 's-schladetzkyj',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     },
     {
       name: 'Eric Kline',
       role: 'Business',
       alias: 's-klineer',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     },
     {
       name: 'Alexander Bowman',
       role: 'Logistics',
       alias: 's-bowmana',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     },
     {
       name: 'Amanda Manea',
       role: 'Scouting',
       alias: 's-maneaa',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     },
     {
       name: 'Timothy Eng',
       role: 'FIRST Lead',
       alias: 's-engt',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     },
     {
       name: 'Anna Trohimovich',
       role: 'Spirit Lead',
       alias: 's-trohimovicha',
-      domain: 'bsd405',
-      tld: 'org'
+      domain: 'bsd405.org'
     }
   ],
   developers: [
