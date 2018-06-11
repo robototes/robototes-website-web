bc1c84432ab95d40e64dd55343a037aa7837179e
dhellstern <dhellstern@outlook.com>
2018-06-11 21:38:52 +0000

    Fixed according to standard

--- routes/index.js
+++ routes/index.js
@@ -16,50 +16,50 @@ router.get('/', async ctx => {
     slideshow
   })
 })
-.get('/about', async ctx => {
-  await ctx.render('about', {
-    seo: {
-      title: 'About | Team 2412 - The Robototes'
-    },
-    robots,
-    sponsors
+  .get('/about', async ctx => {
+    await ctx.render('about', {
+      seo: {
+        title: 'About | Team 2412 - The Robototes'
+      },
+      robots,
+      sponsors
+    })
   })
-})
-.get('/resources', ctx => {
-  ctx.status = 301
-  ctx.redirect('/contact')
-})
-.get('/contact', async ctx => {
-  await ctx.render('contact', {
-    seo: {
-      title: 'Contact Us | Team 2412 - The Robototes'
-    },
-    contact
+  .get('/resources', ctx => {
+    ctx.status = 301
+    ctx.redirect('/contact')
   })
-})
-.get('/calendar', async ctx => {
-  await ctx.render('calendar', {
-    seo: {
-      title: 'Calendar | Team 2412 - The Robototes'
-    }
+  .get('/contact', async ctx => {
+    await ctx.render('contact', {
+      seo: {
+        title: 'Contact Us | Team 2412 - The Robototes'
+      },
+      contact
+    })
   })
-})
-.get('/blog', ctx => {
-  ctx.status = 301
-  ctx.redirect(`https://blog.${nconf.get('DOMAIN')}`)
-})
-.get('/robots.txt', ctx => {
-  ctx.body = ''
-  let robotstxt = []
-  if (nconf.get('DEBUG') != null) robotstxt = seo.robots.debug
-  else robotstxt = seo.robots.production
-  robotstxt.forEach(current => {
-    ctx.body += current.key + ': ' + current.value + '\n'
+  .get('/calendar', async ctx => {
+    await ctx.render('calendar', {
+      seo: {
+        title: 'Calendar | Team 2412 - The Robototes'
+      }
+    })
   })
-})
-.get('/sitemap.xml', ctx => {
-  ctx.type = 'application/xml; charset=utf-8'
-  ctx.body = js2xml.parse('urlset', seo.sitemap)
-})
+  .get('/blog', ctx => {
+    ctx.status = 301
+    ctx.redirect(`https://blog.${nconf.get('DOMAIN')}`)
+  })
+  .get('/robots.txt', ctx => {
+    ctx.body = ''
+    let robotstxt = []
+    if (nconf.get('DEBUG') != null) robotstxt = seo.robots.debug
+    else robotstxt = seo.robots.production
+    robotstxt.forEach(current => {
+      ctx.body += current.key + ': ' + current.value + '\n'
+    })
+  })
+  .get('/sitemap.xml', ctx => {
+    ctx.type = 'application/xml; charset=utf-8'
+    ctx.body = js2xml.parse('urlset', seo.sitemap)
+  })
 
 module.exports = router
