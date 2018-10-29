20abd1301df7809158543fb7b14abf210277e67a
dhellstern <dhellstern@outlook.com>
2018-10-29 07:21:22 +0000

    Updated slideshow with Girls Gen pictures.

--- configs/slideshow.js
+++ configs/slideshow.js
@@ -1,20 +1,11 @@
 module.exports = [
   {
-    filename: 'IMG_141457_scaled.jpg'
+    filename: 'IMG_1026.jpg'
   },
   {
-    filename: 'IMG_141509_scaled.jpg'
+    filename: 'IMG_1027.jpg'
   },
   {
-    filename: 'IMG_141538_scaled.jpg'
-  },
-  {
-    filename: 'IMG_141557_scaled.jpg'
-  },
-  {
-    filename: 'IMG_141740_scaled.jpg'
-  },
-  {
-    filename: 'IMG_141856_scaled.jpg'
+    filename: 'IMG_1028.jpg'
   }
 ]
