fb972236233e575a26d3a9e87e58b8877cd1c6b8
dhellstern <timothyeng36@gmail.com>
2019-03-14 07:45:03 +0000

    Updated slideshow with competition photos.

--- configs/slideshow.js
+++ configs/slideshow.js
@@ -3,15 +3,13 @@ module.exports = [
     filename: 'Team_Photo_2019.jpg'
   },
   {
-    filename: 'Raven0.jpg'
+    filename: 'IMG_0079.JPG'
   },
   {
-    filename: 'Raven1.jpg'
+    filename: 'IMG_0080.JPG'
   },
   {
-    filename: 'Raven2.jpg'
-  },
-  {
-    filename: 'Raven3.jpg'
+    filename: 'IMG_0076.JPG'
   }
 ]
+  
